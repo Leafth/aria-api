1b892e8d82bb69e8728fff2e531d242893e3616e
Felipe Lima <lipezera1836@gmail.com>
2026-03-15 18:39:14 -0300

    chore: adiciona configurações de log e timezone

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module AriaApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.time_zone = "America/Sao_Paulo"
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.api_only = true
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'].split(',')
+    origins ENV["CORS_ORIGINS"].split(",")
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
