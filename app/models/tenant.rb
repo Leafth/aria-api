1c2250fa270b0519a9c97fb5c462fe06607c12c5
Felipe Lima <lipezera1836@gmail.com>
2026-04-20 21:30:25 -0300

    feat: adiciona validações em Tenant model

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,16 @@
 class Tenant < ApplicationRecord
+  has_many :users, dependent: :restrict_with_exception
+
+  enum :status, { active: 0, inactive: 1 }
+
+  before_validation :normalize_slug
+
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
+
+  private
+
+  def normalize_slug
+    self.slug = slug.to_s.parameterize if slug.present?
+  end
 end
