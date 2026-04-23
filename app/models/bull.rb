7af730ccf40a92b140a929a0e5fe4b7cad026800
Felipe Lima <lipezera1836@gmail.com>
2026-04-23 09:08:10 -0300

    feat: adiciona Bull model e suas validações

--- app/models/bull.rb
+++ app/models/bull.rb
@@ -1,4 +1,30 @@
 class Bull < ApplicationRecord
   belongs_to :tenant
-  belongs_to :company
+  belongs_to :company, optional: true
+
+  enum :origin, {
+    local: "local",
+    company: "company"
+  }
+
+  validates :name, presence: true
+  validates :breed, presence: true
+  validates :origin, presence: true
+
+  validates :ear_tag, uniqueness: { scope: :tenant_id }, allow_nil: true
+
+  validates :origin_rules
+
+  private
+
+  def origin_rules
+    if local?
+      errors.add(:company, "Touros locais não devem estar associados a uma empresa") if company_id.present?
+    end
+
+    if company?
+      errors.add(:company, "Touros de empresa devem obrigatoriamente estar associados") if company_id.blank?
+      errors.add(:ear_tag, "Não deve existir para touros de empresa") if ear_tag.present?
+    end
+  end
 end
