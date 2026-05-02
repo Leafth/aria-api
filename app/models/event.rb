07e7370011c89fc8c02d797a7002b3f47428a356
Felipe Lima <lipezera1836@gmail.com>
2026-05-02 09:40:50 -0300

    feat: adiciona model de Event com validações

---
+++ app/models/event.rb
@@ -0,0 +1,24 @@
+class Event < ApplicationRecord
+  belongs_to :cow
+  belongs_to :tenant
+
+  EVENT_TYPES = %w[
+    weighing
+    inactivation
+  ].freeze
+
+  validates :event_type, presence: true, inclusion: { in: EVENT_TYPES }
+  validates :occurred_at, presence: true
+
+  validate :occurred_at_cannot_be_in_future
+
+  private
+
+  def occurred_at_cannot_be_in_future
+    return unless occurred_at.present?
+
+    if occurred_at > Time.current
+      errors.add(:occurred_at, "cannot be in the future")
+    end
+  end
+end
