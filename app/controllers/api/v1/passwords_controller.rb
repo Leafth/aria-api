e48459d7815de94effce90f555a4c0a154348011
Felipe Lima <lipezera1836@gmail.com>
2026-05-07 08:02:47 -0300

    refactor(i18n): usa mensagens do Devise na recuperação de senha

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -8,14 +8,14 @@ module Api
 
         user&.send_reset_password_instructions
 
-        render json: { message: "Se o email existir, você receberá instruções" }, status: :ok
+        render json: { message: I18n.t!("devise.passwords.send_instructions") }, status: :ok
       end
 
       def update
         user = current_tenant.users.reset_password_by_token(reset_params)
 
         if user.errors.empty?
-          render json: { message: "Senha atualizada com sucesso" }, status: :ok
+          render json: { message: I18n.t("devise.passwords.updated_not_active") }, status: :ok
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
