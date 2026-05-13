329a2046923e73dfbeb073c718b37718e38aa0db
Felipe Lima <lipezera1836@gmail.com>
2026-05-13 08:57:52 -0300

    refactor: ajusta array de tipos de eventos reprodutivos

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,24 +13,24 @@ class Event < ApplicationRecord
     calving
   ].freeze
 
-  REPRODUCTIVE_EVENT_TYPES = [
-    EVENT_TYPES[:heat_detection],
-    EVENT_TYPES[:insemination],
-    EVENT_TYPES[:pregnancy_check],
-    EVENT_TYPES[:calving]
+  REPRODUCTIVE_EVENT_TYPES = %w[
+    heat_detection
+    insemination
+    pregnancy_check
+    calving
   ].freeze
 
   scope :reproductive, -> { where(event_type: REPRODUCTIVE_EVENT_TYPES) }
   scope :ordered, -> { order(:occurred_at, :created_at) }
 
-  def reproductive?
-    event_type.in?(REPRODUCTIVE_EVENT_TYPES)
-  end
-
   validates :event_type, presence: true, inclusion: { in: EVENT_TYPES }
   validates :occurred_at, presence: true
 
   validate :occurred_at_cannot_be_in_future
+
+  def reproductive?
+    event_type.in?(REPRODUCTIVE_EVENT_TYPES)
+  end
 
   private
 
