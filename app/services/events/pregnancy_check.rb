4892b5d5644a76b715933748fafdded94165c476
Felipe Lima <lipezera1836@gmail.com>
2026-05-15 08:45:49 -0300

    feat: adiciona service de verificação de gravidez

--- app/services/events/pregnancy_check.rb
+++ app/services/events/pregnancy_check.rb
@@ -1,4 +1,46 @@
 module Events
-  class PregnancyCheck
+  class PregnancyCheck < BaseEvent
+    VALID_RESULTS = %w[
+      positive
+      negative
+    ].freeze
+
+    private
+
+    def event_type
+      "pregnancy_check"
+    end
+
+    def data
+      {
+        result: params.dig(:data, :result)
+      }
+    end
+
+    def validate!
+      super
+
+      Events::ReproductiveTransitionValidator.new(
+        cow: cow,
+        event_type: event_type,
+        data: data,
+      ).validate!
+
+      result = data[:result]
+      raise Events::Error, I18n.t!("events.errors.pregnancy_check.invalid_result") unless VALID_RESULTS.include?(result)
+    end
+
+    def apply!(_event)
+      cow.update!(reproductive_attrs)
+    end
+
+    def reproductive_attrs
+      positive = data[:result] == "positive"
+
+      {
+        reproductive_status: positive ? "pregnant" : "open",
+        pregnancy_confirmed_at: positive ? occurred_at : nil
+      }
+    end
   end
 end
