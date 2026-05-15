2469f05b34b91a72b7511488f0a5d5028f9efc36
Felipe Lima <lipezera1836@gmail.com>
2026-05-15 10:47:42 -0300

    feat: adiciona dados de status reprodutivo no serializer da matriz

--- app/serializers/cow_serializer.rb
+++ app/serializers/cow_serializer.rb
@@ -1,3 +1,14 @@
 class CowSerializer < ActiveModel::Serializer
-    attributes :id, :name, :ear_tag, :birth_date, :breed, :weight, :phase, :active
+  attributes :id,
+    :name,
+    :ear_tag,
+    :birth_date,
+    :breed,
+    :weight,
+    :phase,
+    :reproductive_status,
+    :last_heat_at,
+    :last_insemination_at,
+    :last_calving_at,
+    :active
 end
