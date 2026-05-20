560a96093f5a3a80abec339ada2c491e5da8d766
Felipe Lima <lipezera1836@gmail.com>
2026-05-20 11:00:49 -0300

    feat: adiciona título ao serializer de eventos

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,9 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :event_type, :occurred_at, :observation
+  attributes :id, :event_type, :title, :occurred_at, :observation
+
+  def title
+    I18n.t!("activerecord.attributes.event.event_type.#{object.event_type}")
+  end
 
   def occurred_at
     object.occurred_at&.strftime("%Y-%m-%d")
