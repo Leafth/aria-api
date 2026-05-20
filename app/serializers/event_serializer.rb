819e794fc83b07eed564f47240cef7222772f5c4
Felipe Lima <lipezera1836@gmail.com>
2026-05-20 09:43:44 -0300

    refactor: padroniza retorno de observações dos eventos no serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,11 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :event_type, :occurred_at, :cow_id, :data
+  attributes :id, :event_type, :occurred_at, :observation
 
   def occurred_at
     object.occurred_at&.strftime("%Y-%m-%d")
+  end
+
+  def observation
+    Events::ObservationBuilder.new(object).call
   end
 end
