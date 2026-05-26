53b4b1dd2a66cba69ad54ac3dda237456c5d78b3
Felipe Lima <lipezera1836@gmail.com>
2026-05-26 08:45:08 -0300

    feat: adiciona service para busca e criação de raças

---
+++ app/services/breeds/find_or_create.rb
@@ -0,0 +1,46 @@
+module Breeds
+  class FindOrCreate
+    def initialize(tenant:, breed_id: nil, breed_name: nil)
+      @tenant = tenant
+      @breed_id = breed_id
+      @breed_name = breed_name
+    end
+
+    def call
+      return find_breed if breed_id.present?
+      return find_or_create_breed if breed_name.present?
+
+      raise ActiveRecord::RecordInvalid, invalid_breed_record
+    end
+
+    private
+
+    attr_reader :tenant, :breed_id, :breed_name
+
+    def find_breed
+      tenant.breeds.find(breed_id)
+    end
+
+    def find_or_create_breed
+      tenant.breeds.find_or_create_by!(
+        normalized_name: normalized_breed_name
+      ) do |breed|
+        breed.name = sanitized_breed_name
+      end
+    end
+
+    def sanitized_breed_name
+      breed_name.to_s.strip
+    end
+
+    def normalized_breed_name
+      sanitized_breed_name.parameterize
+    end
+
+    def invalid_breed_record
+      Breed.new.tap do |breed|
+        breed.errors.add(:name, :blank)
+      end
+    end
+  end
+end
