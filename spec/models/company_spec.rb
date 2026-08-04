34aa4613ecc79dcea230ef6a25bb1daae1981c0a
Felipe Lima <lipezera1836@gmail.com>
2026-08-04 08:01:52 -0300

    refactor: adiciona factory de company e refaz seus testes

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,30 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  let(:tenant) { Tenant.create!(name: "Fazenda", slug: "fazenda-teste") }
-
-  def build_company(attrs = {})
-    Company.new({
-      tenant: tenant,
-      name: "Empresa Teste"
-    }.merge(attrs))
-  end
-
   it "é válida com dados válidos" do
-    company = build_company
+    company = build(:company)
 
     expect(company).to be_valid
   end
 
   it "inválida sem tenant" do
-    company = build_company(tenant: nil)
+    company = build(:company, tenant: nil)
 
     expect(company).not_to be_valid
     expect(company.errors[:tenant]).to be_present
   end
 
   it "inválida sem nome" do
-    company = build_company(name: nil)
+    company = build(:company, name: nil)
 
     expect(company).not_to be_valid
     expect(company.errors[:name]).to be_present
